c6aa086aad0dde82c3f4d905bb45d67e364fece1
MarkJhem <162660469+MarkJhem@users.noreply.github.com>
2024-04-01 08:34:06 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -6,20 +6,18 @@ function sumValues(){
     num6=Number(document.formcalc.txtnum6.value);
 
     res=num1/num2;
-    document.formcalc.txtres.value=res;
+    document.formcalc.txtres.value=res.toFixed(2);
  
-    num3 = (res*0.12) * num6;
-    document.formcalc.txtnum3.value=num3;
+    num3 = (res*0.12) * num6; 
+    document.formcalc.txtnum3.value=num3.toFixed(2);
 
     num4 = (res*0.20 * num6);
-    document.formcalc.txtnum4.value=num4;
+    document.formcalc.txtnum4.value=num4.toFixed(2);
 
     num5 = num1-num3-num4;
-    document.formcalc.txtnum5.value=num5;
+    document.formcalc.txtnum5.value=num5.toFixed(2);
 
     num7 = num3 + num4;
-    document.formcalc.txtnum7.value=num7;
-
-
+    document.formcalc.txtnum7.value=num7.toFixed(2);
 }
 
